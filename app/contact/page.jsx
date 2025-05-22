64e96b1b68f81d0e464a420499a2e4d6e78e6d89
Arun kumar <80013677+ATHIRADI@users.noreply.github.com>
2025-05-22 07:06:27 +0530

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -109,7 +109,7 @@ const Contact = () => {
                   <SelectGroup>
                     <SelectLabel>Select a service</SelectLabel>
                     <SelectItem value="web">Web Development</SelectItem>
-                    <SelectItem value="browser">Browser Extensions Development</SelectItem>
+                    <SelectItem value="browser">Browser Extension Development</SelectItem>
                     <SelectItem value="mob">Mobile App Development</SelectItem>
 {/*                     <SelectItem value="opt">Website Optimization</SelectItem>
                     <SelectItem value="bug">Fixing Bugs</SelectItem> */}
